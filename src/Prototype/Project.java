a8ad583e0db62dad8ca6a86cbf7384976c82d3fd
Vladislav <sheba511@mail.ru>
2023-04-12 21:51:34 +0300

    Prototype add

---
+++ src/Prototype/Project.java
@@ -0,0 +1,47 @@
+package Prototype;
+
+public class Project implements Copyable {
+    private int id;
+    private String name;
+    private String source;
+
+    public Project(int id, String name, String source) {
+        this.id = id;
+        this.name = name;
+        this.source = source;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getSource() {
+        return source;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setSource(String source) {
+        this.source = source;
+    }
+
+    @Override
+    public Object copy() {
+        Project copyProject = new Project(id, name, source);
+        return copyProject;
+    }
+
+    public String toString(){
+        return "This project = (id = " + id + ", name = " + name + ", source = " + source + ")";
+    }
+}
