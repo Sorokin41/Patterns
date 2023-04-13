0d1ef7adfaa60311a64de0f13d5545fc7593f828
Vladislav <sheba511@mail.ru>
2023-04-13 22:30:52 +0300

    Decorator added

---
+++ src/Decorator/Client.java
@@ -0,0 +1,10 @@
+package Decorator;
+
+public class Client {
+    public static void main(String[] args) {
+        Developer developer = new JavaTeamLead(new SeniorJavaDeveloper(new JavaDeveloper()));
+        System.out.println(developer.makeJob());
+    }
+
+
+}
---
+++ src/Decorator/SeniorJavaDeveloper.java
@@ -0,0 +1,17 @@
+package Decorator;
+
+public class SeniorJavaDeveloper extends DeveloperDecorator{
+
+    public SeniorJavaDeveloper(Developer developer){
+        super(developer);
+    }
+
+    public String makeCodeReview(){
+        return "SeniorJavaDeveloper make code review. . .";
+    }
+
+    @Override
+    public String makeJob() {
+        return super.makeJob() + makeCodeReview();
+    }
+}
