d4288b20eb226c72b5519e8636bc3e02daa5ff8b
Vladislav <sheba511@mail.ru>
2023-04-15 17:09:35 +0300

    Fliweight added

---
+++ src/Flyweight/Client.java
@@ -0,0 +1,30 @@
+package Flyweight;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Client {
+    public static void main(String[] args) {
+        DeveloperFactory factory = new DeveloperFactory();
+        List<Developer> developers = new ArrayList<>();
+
+        developers.add(factory.getDeveloperBySpecialty("Java"));
+        developers.add(factory.getDeveloperBySpecialty("Java"));
+        developers.add(factory.getDeveloperBySpecialty("Java"));
+        developers.add(factory.getDeveloperBySpecialty("Java"));
+        developers.add(factory.getDeveloperBySpecialty("Java"));
+        developers.add(factory.getDeveloperBySpecialty("Java"));
+        developers.add(factory.getDeveloperBySpecialty("Cpp"));
+        developers.add(factory.getDeveloperBySpecialty("Cpp"));
+        developers.add(factory.getDeveloperBySpecialty("Cpp"));
+        developers.add(factory.getDeveloperBySpecialty("Cpp"));
+        developers.add(factory.getDeveloperBySpecialty("Cpp"));
+
+
+        for (Developer current : developers){
+            if (current != null)
+            current.writeCode();
+        }
+        System.out.println(factory.developers.toString());
+    }
+}
