5a3e4949685463671470c1b65f61fec0fd9db04b
Vladislav <sheba511@mail.ru>
2023-04-18 12:49:51 +0300

    Interpreter added

---
+++ src/Behavioral/Interpreter/TerminalExpression.java
@@ -0,0 +1,16 @@
+package Behavioral.Interpreter;
+
+public class TerminalExpression implements Expression {
+    String data;
+
+    public TerminalExpression(String data) {
+        this.data = data;
+    }
+
+    @Override
+    public boolean interpret(String context) {
+        if (context.contains(data))
+            return true;
+        return false;
+    }
+}
